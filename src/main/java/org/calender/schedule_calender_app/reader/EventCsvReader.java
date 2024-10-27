b8951092f47818287bcfb9ddf0a252ea9457e243
윤설이 <olerlmin1234@gmail.com>
2024-10-27 23:17:53 +0900

    .csv 추가

---
+++ src/main/java/org/calender/schedule_calender_app/reader/EventCsvReader.java
@@ -0,0 +1,69 @@
+package org.calender.schedule_calender_app.reader;
+
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+
+public class EventCsvReader {
+    public List<org.calender.schedule_calender_app.event.Meeting> readMeetings(String path) throws IOException {
+        List<org.calender.schedule_calender_app.event.Meeting> result = new ArrayList<>();
+
+        // 데이터를 읽는 부분
+        List<String[]> read = readAll(path);
+        for (int i = 0; i < read.size(); i++) {
+            // 헤더는 스킵 - 메서드로 분리
+            if (skipHeader(i)) {
+                continue;
+            }
+
+            String[] each = read.get(i);
+
+            result.add(
+                    new org.calender.schedule_calender_app.event.Meeting(
+                            Integer.parseInt(each[0]),                          // ID
+                            each[2],                                            // 제목
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(each[6], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                                    ZoneId.of("Asia/Seoul")
+                            ),                                                  // 시작 시간
+                            ZonedDateTime.of(
+                                    LocalDateTime.parse(each[7], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")),
+                                    ZoneId.of("Asia/Seoul")
+                            ),                                                  // 종료 시간
+                            new HashSet<>(Arrays.asList(each[3].replace("\"", "").split(","))), // 참여자
+                            each[4],                                            // 회의실
+                            each[5]                                             // 안건
+                    )
+            );
+
+        }
+        // meeting 으로 변환하는 부분
+
+
+        return result;
+    }
+
+    private static boolean skipHeader(int i) {
+        return i == 0;
+    }
+
+    private List<String[]> readAll(String path) throws IOException {
+        InputStream in = getClass().getResourceAsStream(path);
+        InputStreamReader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+
+        CSVReader csvReader = new CSVReader(reader);
+        return csvReader.readAll();
+
+    }
+}
