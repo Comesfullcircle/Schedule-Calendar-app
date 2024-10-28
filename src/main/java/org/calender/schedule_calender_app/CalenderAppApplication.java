28cb0f9392d26a5b6d13a413fda2a01c1d75eb58
윤설이 <olerlmin1234@gmail.com>
2024-10-28 16:28:25 +0900

    csv로 데이터 등록

--- src/main/java/org/calender/schedule_calender_app/CalenderAppApplication.java
+++ src/main/java/org/calender/schedule_calender_app/CalenderAppApplication.java
@@ -11,32 +11,37 @@ import java.util.List;
 
 public class CalenderAppApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Schedule schedule = new Schedule();
         EventCsvReader csvReader = new EventCsvReader();
         String meetingCsvPath = "/data/meeting.csv";
 
-        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
-        meetings.forEach(schedule::add);
+        try {
+            List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+            meetings.forEach(schedule::add);
 
-        Meeting meeting = meetings.get(0);
-        meeting.print();
+            Meeting meeting = meetings.get(0);
+            meeting.print();
 
-        System.out.println("수정 후 ... ");
-        meeting.validateAndUpdate(
-                new UpdateMeeting(
-                        "new title",
-                        ZonedDateTime.now(),
-                        ZonedDateTime.now().plusHours(1),
-                        null,
-                        "A",
-                        "new agenda"
-                )
-        );
-        meeting.print();
+            System.out.println("수정 후 ... ");
+            meetings.get(0).validateAndUpdate(
+                    new UpdateMeeting(
+                            "new title",
+                            ZonedDateTime.now(),
+                            ZonedDateTime.now().plusHours(1),
+                            null,
+                            "A",
+                            "new agenda"
+                    )
+            );
+            meeting.print();
 
+            schedule.printAll();
 
-        // schedule.printAll();
+        } catch (IOException e) {
+            System.out.println("CSV 파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
+        }
 
+        schedule.printAll();
     }
 }
