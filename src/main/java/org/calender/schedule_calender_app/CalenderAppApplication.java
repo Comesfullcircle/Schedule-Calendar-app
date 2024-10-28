df89bfb0901d576998ca404ffac77dcb2ea26309
윤설이 <olerlmin1234@gmail.com>
2024-10-28 16:43:09 +0900

    데이터 수정 로직 생성

--- src/main/java/org/calender/schedule_calender_app/CalenderAppApplication.java
+++ src/main/java/org/calender/schedule_calender_app/CalenderAppApplication.java
@@ -1,26 +1,42 @@
 package org.calender.schedule_calender_app;
 
 import org.calender.schedule_calender_app.event.*;
+import org.calender.schedule_calender_app.event.update.UpdateMeeting;
 import org.calender.schedule_calender_app.reader.EventCsvReader;
 
 
 import java.io.IOException;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 public class CalenderAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Schedule schedule = new Schedule();
         EventCsvReader csvReader = new EventCsvReader();
         String meetingCsvPath = "/data/meeting.csv";
 
-        try {
-            List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
-            meetings.forEach(schedule::add);
-        } catch (IOException e) {
-            System.out.println("CSV 파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
-        }
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(schedule::add);
 
-        schedule.printAll();
+        Meeting meeting = meetings.get(0);
+        meeting.print();
+
+        System.out.println("수정 후 ... ");
+        meeting.validateAndUpdate(
+                new UpdateMeeting(
+                        "new title",
+                        ZonedDateTime.now(),
+                        ZonedDateTime.now().plusHours(1),
+                        null,
+                        "A",
+                        "new agenda"
+                )
+        );
+        meeting.print();
+
+
+        // schedule.printAll();
+
     }
 }
