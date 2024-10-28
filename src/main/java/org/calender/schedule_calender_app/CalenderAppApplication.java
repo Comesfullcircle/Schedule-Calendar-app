8fb37d0761ce5825fb48d50cf573931640183974
윤설이 <olerlmin1234@gmail.com>
2024-10-28 16:43:09 +0900

    데이터 수정 로직 생성

--- src/main/java/org/calender/schedule_calender_app/CalenderAppApplication.java
+++ src/main/java/org/calender/schedule_calender_app/CalenderAppApplication.java
@@ -11,37 +11,32 @@ import java.util.List;
 
 public class CalenderAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Schedule schedule = new Schedule();
         EventCsvReader csvReader = new EventCsvReader();
         String meetingCsvPath = "/data/meeting.csv";
 
-        try {
-            List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
-            meetings.forEach(schedule::add);
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(schedule::add);
 
-            Meeting meeting = meetings.get(0);
-            meeting.print();
+        Meeting meeting = meetings.get(0);
+        meeting.print();
 
-            System.out.println("수정 후 ... ");
-            meetings.get(0).validateAndUpdate(
-                    new UpdateMeeting(
-                            "new title",
-                            ZonedDateTime.now(),
-                            ZonedDateTime.now().plusHours(1),
-                            null,
-                            "A",
-                            "new agenda"
-                    )
-            );
-            meeting.print();
+        System.out.println("수정 후 ... ");
+        meeting.validateAndUpdate(
+                new UpdateMeeting(
+                        "new title",
+                        ZonedDateTime.now(),
+                        ZonedDateTime.now().plusHours(1),
+                        null,
+                        "A",
+                        "new agenda"
+                )
+        );
+        meeting.print();
 
-            schedule.printAll();
 
-        } catch (IOException e) {
-            System.out.println("CSV 파일을 읽는 중 오류가 발생했습니다: " + e.getMessage());
-        }
+        // schedule.printAll();
 
-        schedule.printAll();
     }
 }
